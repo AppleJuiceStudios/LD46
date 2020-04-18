313e1aec9e432b6027f3485d274a57089fffec5a
Florian Diederichs <florian.diederichs@online.de>
2020-04-18 18:35:28 +0200

    added simple level timer

--- LD46/levels/PowerBar.gd
+++ LD46/levels/PowerBar.gd
@@ -7,12 +7,13 @@ func _ready() -> void:
 	_on_power_updated()
 	
 
-func _on_Timer_timeout() -> void:
-	if PlayerData.power < 100:
-		PlayerData.power += 1
-
 func _on_power_updated():
 	value = PlayerData.power
 	
 func _on_max_power_updated():
 	max_value = PlayerData.max_power
+
+
+func _on_PowerTimer_timeout() -> void:
+	if PlayerData.power < 100:
+		PlayerData.power += 1

88f790c93a489d43d66ed41a6150a9d6af811234
TobiasBodewig <tbodewig@web.de>
2020-04-20 19:02:15 +0200

    Spawner also changes breaching speed. And fixed bug with spawner.

--- LD46/objects/Spawner.gd
+++ LD46/objects/Spawner.gd
@@ -5,6 +5,9 @@ export var intervall_end : = 4.0
 export var intervall_slope : = -0.3
 export var motivation_start : = 60
 export var motivation_slope : = 2
+export var breaching_speed_start : = 1.5
+export var breaching_speed_end : = 0.1
+export var breaching_speed_slope : = -0.025
 
 export var goal_path : NodePath
 export var nav_2d_path : NodePath
@@ -24,17 +27,24 @@ func _process(delta: float) -> void:
 	nav_2d.add_heat(global_position + Vector2.DOWN, delta * 10)
 	_cool_down -= delta
 	if _cool_down <= 0.0:
-		spawn_intruder(motivation_start + motivation_slope * _spawn_counter)
+		var motivation = motivation_start + motivation_slope * _spawn_counter
 		var time : = intervall_start + intervall_slope * _spawn_counter
 		if time < intervall_end:
 			time = intervall_end
+		var breaching_speed = breaching_speed_start + breaching_speed_slope * _spawn_counter
+		if breaching_speed < breaching_speed_end:
+			breaching_speed = breaching_speed_end
+		spawn_intruder(motivation, breaching_speed)
 		_cool_down += time
 
-func spawn_intruder(motivation : float) -> void:
+func spawn_intruder(motivation : float, breaching_speed : float) -> void:
 	var pos : = Vector2(rand_range(-spawn_area_X, spawn_area_X), rand_range(-spawn_area_Y, spawn_area_Y))
 	var intruder : Node = intruder_scene.instance()
 	intruder.goal_path = goal_path
 	intruder.nav_2d_path = nav_2d_path
 	intruder.home_path = get_path()
 	intruder.global_position = global_position + pos
+	intruder.max_motivation = motivation
+	intruder.breaching_speed = breaching_speed
 	get_parent().add_child(intruder)
+	_spawn_counter += 1
